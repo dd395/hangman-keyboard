91045f5550ec1b4bb435f546a8f074693a491ef5
dd395 <drichardssmith@my.wctc.edu>
2020-12-05 22:08:49 -0600

    Initial commit

---
+++ code.js
@@ -0,0 +1,106 @@
+
+//create an array of words from 12 Days of Christmas
+        let words = ["drummers", "pipers", "lords", "ladies", "maids", "swans", "geese", "rings", "birds", "hens", "doves", "partridge"];
+
+
+        let answer = '';
+        let maxWrong = 6;
+        let mistakes = 0;
+        let guessed = [];
+        let wordStatus = null;
+
+        function randomWord() {
+
+            answer = words[Math.floor(Math.random() * words.length)];
+            // alert(answer);
+        }
+
+
+        function generateButtons() {
+            let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+                `
+            <button
+            class="btn btn-lg btn-primary m-2"
+            id= '` + letter + `'
+            onClick="handleGuess('` + letter + `')"
+            >
+` + letter + `
+</button>
+`).join('');
+            document.getElementById('keyboard').innerHTML = buttonsHTML;
+        }
+
+
+        function handleGuess(chosenLetter) {
+
+            guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+            document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+            //alert(answer);
+
+            if (answer.indexOf(chosenLetter) >= 0) {
+                guessedWord();
+                checkIfGameWon();
+            } else if (answer.indexOf(chosenLetter) === -1) {
+                mistakes++;
+                updateMistakes();
+                checkIfGameLost();
+            }
+        }
+
+
+        function checkIfGameWon() {
+            if (wordStatus === answer) {
+                document.getElementById('keyboard').innerHTML = "You Won!!!";
+            }
+        }
+
+        function checkIfGameLost() {
+            if (mistakes === maxWrong) {
+                document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
+                document.getElementById('keyboard').innerHTML = "You Lost!!!";
+            }
+        }
+
+        function guessedWord() {
+            wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " - ")).join('');
+            document.getElementById('wordSpotlight').innerHTML = wordStatus;
+        }
+
+        function updateMistakes() {
+            document.getElementById('mistakes').innerHTML = mistakes;
+        }
+
+        function reset() {
+            mistakes = 0;
+            guessed = [];
+
+
+            randomWord();
+            guessedWord();
+            updateMistakes();
+            generateButtons();
+        }
+
+        {
+
+            document.getElementById('maxWrong').innerHTML = maxWrong;
+
+
+        }
+
+
+        randomWord();
+        generateButtons();
+        guessedWord();
+        reset();
+
+
+
+
+
+
+
+
+
+
